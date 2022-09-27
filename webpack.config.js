644ab836ff91eb4669bc11a7efa985a81bd271d3
Camilovelag <camilo.vela.galindo@gmail.com>
2022-09-27 11:36:52 -0500

    Setup local server

--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,28 @@ module.exports = {
     index: './src/index.js',
     print: './src/print.js',
   },
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
-   }),
+      template: './src/index.html',
+    }),
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
