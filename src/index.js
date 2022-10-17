a4c3bb0cb78a4bdb59eeacc5800e4ef70b20b750
Camilovelag <camilo.vela.galindo@gmail.com>
2022-10-17 13:00:32 -0500

    Setup layout (index + popup)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import _ from 'lodash';
 import './style.css';
-import 'bootstrap';
 
- function component() {
-   const element = document.createElement('div');
+const submitList = document.querySelectorAll('.submit');
+const popup = document.querySelector('.popup');
+const closeBtn = document.getElementById('close');
 
-   // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+// display popup and get id of retrieved item
+submitList.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const input = e.target.id;
+    const id = input.split('-')[1];
+    console.log(id);
+    popup.style.display = 'flex';
+  });
+});
 
-   return element;
- }
-
- document.body.appendChild(component());
+// close popup
+closeBtn.addEventListener('click', () => {
+  popup.style.display = 'none';
+});
